9987c0a7e91f2826086c120a2c026fe3e1a2120e
Damir Afliatonov <88425319+Probirochniy@users.noreply.github.com>
2022-03-21 13:50:30 +0300

    Update Bird.java

--- src/com/muwaffaq/innopolis/solid/LSP_IS/wrong/Bird.java
+++ src/com/muwaffaq/innopolis/solid/LSP_IS/wrong/Bird.java
@@ -2,9 +2,21 @@ package com.muwaffaq.innopolis.solid.LSP_IS.wrong;
 //❌❌❌
 // how many SOLID Does it Violates ?
 // Fix it.
- interface Bird {
+ interface BirdFlyable {
     void fly();
     void eat();
     void hatch();
+}
+
+interface BirdSwimmable{
+    void eat();
+    void hatch();
     void swim();
+}
+
+interface BirdAllMighty{
+    void eat();
+    void hatch();
+    void swim();
+    void fly();
 }
